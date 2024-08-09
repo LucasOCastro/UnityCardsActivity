3b17cab92c949aa1672ff622607cfc7829639273
lucas castro <lucas_o__castro@hotmail.com>
2024-08-09 00:19:28 -0300

    Basic CardLine organizer script and scene

---
+++ Assets/Scripts/CardGameActivity/CardLine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardGameActivity
+{
+    public class CardLine : MonoBehaviour
+    {
+        [SerializeField] private float cardWidth;
+        [SerializeField] private float cardSpacing;
+    
+        private readonly List<Transform> _cards = new();
+
+        private void Awake()
+        {
+            //TODO debug
+            _cards.AddRange(transform.GetComponentsInChildren<Transform>());
+            _cards.Remove(transform);
+        }
+
+        private void Update()
+        {
+            PositionCards();
+        }
+
+        private void PositionCards()
+        {
+            if (_cards.Count == 0) return;
+            
+            float totalWidth = _cards.Count * cardWidth + (_cards.Count - 1) * cardSpacing;
+            float leftmostX = (-totalWidth + cardWidth) * .5f;
+            Vector2 pos = new(leftmostX, 0);
+            Vector2 offsetBetweenCards = new(cardWidth + cardSpacing, 0);
+            foreach (var card in _cards)
+            {
+                card.transform.localPosition = pos;
+                pos += offsetBetweenCards;
+            }
+        }
+    }
+}
