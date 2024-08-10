4e9304f744dc1dfe251a4d73501e21e9fb5a723d
lucas castro <lucas_o__castro@hotmail.com>
2024-08-10 11:56:42 -0300

    Adds a stack of cards to the scene

---
+++ Assets/Scripts/CardGameActivity/CardStack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CardGameActivity
+{
+    public class CardStack : MonoBehaviour
+    {
+        [SerializeField] private CardDeck deck;
+        [SerializeField] private Card cardPrefab;
+
+        public Card GetCard()
+        {
+            if (deck == null) return null;
+
+            var def = deck.GetRandomCardDef();
+            if (def == null) return null;
+
+            var card = Instantiate(cardPrefab);
+            card.Definition = def;
+            return card;
+        }
+    }
+}
