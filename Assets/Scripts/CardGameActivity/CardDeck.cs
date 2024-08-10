3c935d1c4c3878314e120f5833cbad793d1f5f60
lucas castro <lucas_o__castro@hotmail.com>
2024-08-10 11:56:04 -0300

    Creates a deck of cards

---
+++ Assets/Scripts/CardGameActivity/CardDeck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardGameActivity
+{
+    [CreateAssetMenu(menuName = "Create Card Deck", fileName = "New CardDeck", order = 0)]
+    public class CardDeck : ScriptableObject
+    {
+        [SerializeField] private List<CardDef> cards = new();
+        
+        public List<CardDef> Cards => cards;
+
+        public CardDef GetRandomCardDef() => cards.Count > 0 ? cards[Random.Range(0, cards.Count)] : null;
+    }
+}
