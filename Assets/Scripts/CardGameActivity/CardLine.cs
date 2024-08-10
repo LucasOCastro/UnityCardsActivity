94a6393f542e7826374766c887b0dd5487fada48
lucas castro <lucas_o__castro@hotmail.com>
2024-08-09 23:49:26 -0300

    Cards can now be moved within lines

--- Assets/Scripts/CardGameActivity/CardLine.cs
+++ Assets/Scripts/CardGameActivity/CardLine.cs
@@ -11,59 +11,119 @@ namespace CardGameActivity
         [SerializeField] private float cardWidth;
         [SerializeField] private float cardSpacing;
         [SerializeField] private float hoverCardHeightOffset;
-    
+        [SerializeField] private float dragCardHeightOffset;
         [SerializeField] private List<Card> cards = new();
-
+        
+        /// <summary> Índice da carta a qual o mouse está em cima, deve ser destacada. </summary>
         private int _highlightedIndex = -1;
         
+        private Card _dragging;
+        private float _draggedLocalX;
+        
+        /// <summary>
+        /// A largura total, da borda esquerda da primeira carta à borda direita da última carta.
+        /// </summary>
         private float TotalWidth => cards.Count * cardWidth + (cards.Count - 1) * cardSpacing;
 
+        /// <summary>
+        /// A distância entre um centro da carta e outro.
+        /// </summary>
+        private float OffsetBetweenCardCenters => cardWidth + cardSpacing;
+
+        /// <summary>
+        /// O centro da carta de índice 0.
+        /// </summary>
+        private float LeftmostCenterX => (-TotalWidth + cardWidth) * .5f; 
+        
+        /// <returns>
+        /// O X em posição local da carta no índice passado.
+        /// </returns>
+        private float GetCenterXForCardIndex(int cardIndex) => LeftmostCenterX + cardIndex * OffsetBetweenCardCenters;
+        
         private void Update()
         {
             PositionCards();
             _highlightedIndex = -1;
         }
 
+        /// <summary>
+        /// Atualiza a posição de cada carta contida na linha,
+        /// aplicando deslocamentos de destaque e seleção.
+        /// </summary>
         private void PositionCards()
         {
             if (cards.Count == 0) return;
             
-            // transform.forward = lado oposto às frentes das cartas
-            // A frente das cartas está virada para -transform.forward
-            float leftmostX = (-TotalWidth + cardWidth) * .5f;
-            Vector3 pos = new(leftmostX, 0, 0);
-            Vector3 offsetBetweenCards = new(cardWidth + cardSpacing, 0, 0);
+            Vector3 pos = new(LeftmostCenterX, 0, 0);
+            Vector3 offsetBetweenCards = new(OffsetBetweenCardCenters, 0, 0);
             for (int i = 0; i < cards.Count; i++)
             {
                 var card = cards[i].transform;
                 
-                card.localPosition = pos;
-                if (i == _highlightedIndex) 
+                //Se tem uma carta sendo arrastada, afasta as cartas em volta pra mostrar onde vai ser inserido.
+                if (!_dragging)
+                    card.localPosition = pos;
+                else if (pos.x < _draggedLocalX)
+                    card.localPosition = pos - new Vector3(cardWidth * .5f, 0, 0);
+                else if (pos.x > _draggedLocalX)
+                    card.localPosition = pos + new Vector3(cardWidth * .5f, 0, 0);
+                
+                //Se o mouse está em cima da carta, levanta ela um pouco pra indicar que vai ser selecionada.
+                if (i == _highlightedIndex)
                     card.position -= transform.forward * hoverCardHeightOffset;
+                
+                // transform.forward = lado oposto às frentes das cartas
+                // A frente das cartas deve estar virada para -transform.forward
                 card.forward = transform.forward;
 
                 pos += offsetBetweenCards;
             }
         }
 
+        #region MOUSE_HANDLING
+        
         public void HandleMouse(Vector3 collisionPoint, bool click)
         {
             if (cards.Count == 0) return;
             
-            // Calcular o índice da carta baseado na posição relativa do mouse à linha
+            // Calcular o índice da carta baseado na posição do mouse na linha
             float localX = transform.InverseTransformPoint(collisionPoint).x;
             float normalizedX = localX / TotalWidth + .5f;
             int index = (int)(normalizedX * cards.Count);
+
+            if (_dragging)
+                HandleDragCard(localX, index, click);
+            else 
+                HandleHover(index, click);
+        }
+        
+        private void HandleDragCard(float localX, int closestIndex, bool click) 
+        {
+            _draggedLocalX = localX;
+            _dragging.transform.localPosition = new(localX, 0, -dragCardHeightOffset);
+            if (!click) return;
+            
+            //Se o mouse está à direita da carta mais próxima, vai inserir no próximo índice
+            int newIndex = Mathf.Clamp(closestIndex, 0, cards.Count - 1);
+            if (localX > GetCenterXForCardIndex(closestIndex))
+                newIndex++;
+            
+            cards.Insert(newIndex, _dragging); 
+            _dragging = null;
+        }
+
+        private void HandleHover(int index, bool click)
+        {
             if (index < 0 || index >= cards.Count) return;
 
-            // Destacar a carta e executar a ação de click
-            _highlightedIndex = index;
             if (click)
             {
-                //TODO debug
-                Destroy(cards[index].gameObject);
+                _dragging = cards[index];
                 cards.RemoveAt(index);
             }
+            else _highlightedIndex = index;
         }
+        
+        #endregion
     }
 }
